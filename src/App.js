cd50f192629299b3fc7c8327d38dc042948b325e
Dawid Pers <dawid.g657@gmail.com>
2023-01-07 19:53:31 +0100

    Create register page, user profile page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import BookList from "./views/BookList/BookList";
-import Profile from "./views/Profile";
 import Login from "./views/Login/Login";
 import React from "react";
-import {LocalizationProvider} from "@mui/x-date-pickers";
-import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import Register from "./views/Login/Register";
 import Book from "./views/Book/Book";
 import AdminPanel from "./views/AdminPanel/AdminPanel";
+import Profile from "./views/Profile";
 
 function App() {
-  return (
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-          <BrowserRouter>
-              <Routes>
-                  <Route element={<Layout container={true} />}>
-                      <Route path="/" element={<BookList />}/>
-                      <Route path="/book/:bookId" element={<Book />}/>
-                  </Route>
-                  <Route element={<Layout container={false} />}>
-                      <Route path="/profile" element={<Profile />}/>
-                      <Route path="/admin" element={<Navigate to="/admin/users" />} />
-                      <Route path="/admin/:subPage" element={<AdminPanel />}/>
-                  </Route>
-                  <Route path="/login" element={<Login />}/>
-                  <Route path="/register" element={<Register />} />
-              </Routes>
-          </BrowserRouter>
-      </LocalizationProvider>
-  );
+    return (
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Layout container={true} />}>
+                        <Route path="/" element={<BookList />} />
+                        <Route path="/book/:bookId" element={<Book />} />
+                    </Route>
+                    <Route element={<Layout container={false} />}>
+                        <Route
+                            path="/profile"
+                            element={<Navigate to="/profile/info" />}
+                        />
+                        <Route path="/profile/:subPage" element={<Profile />} />
+                        <Route
+                            path="/admin"
+                            element={<Navigate to="/admin/users" />}
+                        />
+                        <Route
+                            path="/admin/:subPage"
+                            element={<AdminPanel />}
+                        />
+                    </Route>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
+        </LocalizationProvider>
+    );
 }
 
 export default App;
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,11 +1,31 @@
-import React from 'react';
-
+import { People, BookmarksOutlined, Bookmarks } from "@mui/icons-material";
+import React from "react";
+import Borrowings from "../components/Profile/Borrowings";
+import UserReservations from "../components/Profile/UserReservations";
+import UserInfo from "./../components/Profile/UserInfo";
+import PageWithMenu from "../components/PageWithMenu/PageWithMenu";
+const subPages = [
+    {
+        name: "Profil",
+        path: "info",
+        icon: <People />,
+        component: UserInfo,
+    },
+    {
+        name: "Rezerwacje",
+        path: "reservations",
+        icon: <BookmarksOutlined />,
+        component: UserReservations,
+    },
+    {
+        name: "Wypożyczenia",
+        path: "borrowings",
+        icon: <Bookmarks />,
+        component: Borrowings,
+    },
+];
 const Profile = () => {
-    return (
-        <div>
-            PROFILE
-        </div>
-    );
+    return <PageWithMenu subPages={subPages} />;
 };
 
 export default Profile;
