dcff54749e25a2e3dabbf4135aa5e9e2bf184c57
Dawid Pers <dawid.g657@gmail.com>
2023-01-13 22:21:09 +0100

    Add reservation page to profile page

--- src/components/Profile/UserReservations.js
+++ src/components/Profile/UserReservations.js
@@ -1,16 +1,163 @@
-import React from "react";
-import { Grid, Typography, Box } from "@mui/material";
+import React, { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+    TableHead,
+    Grid,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    Tooltip,
+    Typography,
+} from "@mui/material";
+import FullWidthButton from "./../FullWidthButton";
+import CustomModal from "./../CustomModal";
+import ReservationStatusChip from "./../AdminPanel/ReservationStatusChip";
+import useFindUser from "./../../hooks/useFindUser";
+import useFindBook from "./../../hooks/useFindBook";
+import CustomTooltip from "./../CustomTooltip";
+import DoDisturbOnOutlinedIcon from "@mui/icons-material/DoDisturbOnOutlined";
+import userReservations from "./../../mock/userReservations";
+const UserReservationContext = createContext({});
+
+const UserReservationModal = ({
+    title,
+    leftAction,
+    leftText,
+    rightAction,
+    rightText,
+}) => {
+    return (
+        <Grid container justifyContent="center" gap={3}>
+            <Grid item xs={12}>
+                <Typography align="center" variant="h6">
+                    {title}
+                </Typography>
+            </Grid>
+            <Grid item container xs={12} md={8} justifyContent="space-between">
+                <Grid item xs={5}>
+                    <FullWidthButton variant="outlined" onClick={leftAction}>
+                        {leftText}
+                    </FullWidthButton>
+                </Grid>
+                <Grid item xs={5}>
+                    <FullWidthButton variant="contained" onClick={rightAction}>
+                        {rightText}
+                    </FullWidthButton>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+const UserReservationDeleteModal = ({ open, setOpen, reservationId }) => {
+    const deleteReservation = () => {
+        console.log("delete");
+        setOpen(false);
+    };
+
+    return (
+        <CustomModal setOpen={setOpen} open={open}>
+            <UserReservationModal
+                title="Czy na pewno chcesz anulować rezerwację?"
+                rightText="Anuluj"
+                leftText="Zamknij"
+                leftAction={() => setOpen(false)}
+                rightAction={deleteReservation}
+            />
+        </CustomModal>
+    );
+};
+
+const UserReservationRow = ({ reservation }) => {
+    const user = useFindUser(reservation.userId);
+    const book = useFindBook(reservation.bookId);
+    const [modal, setModal] = useState(false);
+    const [deleteModal, setDeleteModal] = useState(false);
+    const navigate = useNavigate();
+    if (!user || !book) return <></>;
+    const selectBook = (bookId) => {
+        navigate(`/book/${bookId}`);
+    };
+    return (
+        <>
+            <TableRow
+                hover
+                sx={{ cursor: "pointer" }}
+                onClick={() => selectBook(reservation.bookId)}
+            >
+                <TableCell width="20%">
+                    <CustomTooltip content={book.category} />
+                </TableCell>
+                <TableCell width="20%">
+                    <CustomTooltip content={book.title} />
+                </TableCell>
+                <TableCell width="10%">
+                    <CustomTooltip content={reservation.validTill} />
+                </TableCell>
+                <TableCell width="10%">
+                    <Grid container justifyContent="center">
+                        <ReservationStatusChip status={reservation.status} />
+                    </Grid>
+                </TableCell>
+
+                <TableCell width="10%">
+                    <Grid
+                        container
+                        justifyContent="center"
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        {reservation.status === "RESERVED" && (
+                            <Tooltip title={`Anuluj rezerwację`}>
+                                <DoDisturbOnOutlinedIcon
+                                    onClick={() => setDeleteModal(true)}
+                                />
+                            </Tooltip>
+                        )}
+                    </Grid>
+                </TableCell>
+            </TableRow>
+            <UserReservationContext.Provider
+                value={{
+                    modal,
+                    setModal,
+                }}
+            >
+                <UserReservationDeleteModal
+                    open={deleteModal}
+                    setOpen={setDeleteModal}
+                    reservationId={reservation._id}
+                />
+            </UserReservationContext.Provider>
+        </>
+    );
+};
 
 const UserReservations = () => {
     return (
-        <Grid container spacing={3} pt={3}>
-            <Grid container item xs={4} direction="column">
-                <Typography variant="h5" pb={3}>
-                    Dane Osobowe
-                </Typography>
-                <Box sx={{ p: 2, border: "0.5px solid grey" }}></Box>
-            </Grid>
-        </Grid>
+        <TableContainer component={Paper}>
+            <Table aria-label="Lista rezerwacji" sx={{ tableLayout: "fixed" }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Wydawnictwo</TableCell>
+                        <TableCell>Tytuł</TableCell>
+                        <TableCell>Data rezerwacji</TableCell>
+                        <TableCell align="center">Status rezerwacji</TableCell>
+                        <TableCell align="center">Usuń rezerwację</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {userReservations.map((reservation, i) => (
+                        <UserReservationRow
+                            key={`reservation-${i}`}
+                            reservation={reservation}
+                        />
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
 
